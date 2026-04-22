b8e2a1c332589914d6b1355fe1c4ccacfd47b4f7
Yeremia Yoga <yogayeremia@gmail.com>
2026-04-22 20:44:13 +0700

    first commit

--- scripts/helper.js
+++ scripts/helper.js
@@ -1,2 +1,2 @@
-export const API_BASE_URL = "http://localhost:19984";
-// export const API_BASE_URL = "https://azcn87b0k85drpfp.phanneldeliver.my.id";
+// export const API_BASE_URL = "http://localhost:19984";
+export const API_BASE_URL = "https://azcn87b0k85drpfp.phanneldeliver.my.id";
